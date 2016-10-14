f5a4b808dc1dc47101659ad2a02d66de0717239d
Federico Sassone <fsassone@ws10.labo5>
2016-10-13 21:08:49 -0300

    first commit

---
+++ tp3/src/kernel.asm
@@ -0,0 +1,116 @@
+; ** por compatibilidad se omiten tildes **
+; ==============================================================================
+; TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+; ==============================================================================
+
+%include "imprimir.mac"
+
+global start
+
+
+;; GDT
+extern GDT_DESC
+
+;; IDT
+extern IDT_DESC
+extern idt_inicializar
+
+;; PIC
+extern resetear_pic
+extern habilitar_pic
+
+
+;; Saltear seccion de datos
+jmp start
+
+;;
+;; Seccion de datos.
+;; -------------------------------------------------------------------------- ;;
+iniciando_mr_msg db     'Iniciando kernel (Modo Real)...'
+iniciando_mr_len equ    $ - iniciando_mr_msg
+
+iniciando_mp_msg db     'Iniciando kernel (Modo Protegido)...'
+iniciando_mp_len equ    $ - iniciando_mp_msg
+
+;;
+;; Seccion de código.
+;; -------------------------------------------------------------------------- ;;
+
+;; Punto de entrada del kernel.
+BITS 16
+start:
+    ; Deshabilitar interrupciones
+    cli
+
+    ; xchg bx, bx
+
+    ; Imprimir mensaje de bienvenida
+    imprimir_texto_mr iniciando_mr_msg, iniciando_mr_len, 0x07, 0, 0
+
+
+    ; habilitar A20
+    call habilitar_A20
+    ; cargar la GDT
+    LGDT [GDT_DESC]
+    
+    ; setear el bit PE del registro CR0
+    xchg bx,bx
+    mov  eax, cr0       
+    or eax, 1
+    mov cr0, eax
+    xchg bx,bx  
+    ; pasar a modo protegido
+    jmp 0x98:modoprotegido
+
+BITS 32    
+   modoprotegido:    
+    xchg bx,bx  
+    ; acomodar los segmentos
+    xor eax, eax
+    mov ax, 0x88        
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov gs, ax
+
+    ; setear la pila
+    mov esp, 0x27000
+
+    ; pintar pantalla, todos los colores, que bonito!
+
+
+    ; inicializar el manejador de memoria
+
+    ; inicializar el directorio de paginas
+
+    ; inicializar memoria de tareas
+
+    ; habilitar paginacion
+
+    ; inicializar tarea idle
+
+    ; inicializar todas las tsss
+
+    ; inicializar entradas de la gdt de las tsss
+
+    ; inicializar el scheduler
+
+    ; inicializar la IDT
+
+    ; configurar controlador de interrupciones
+
+    ; cargar la tarea inicial
+
+    ; saltar a la primer tarea
+
+    ; Ciclar infinitamente (por si algo sale mal...)
+    mov eax, 0xFFFF
+    mov ebx, 0xFFFF
+    mov ecx, 0xFFFF
+    mov edx, 0xFFFF
+    jmp $
+    jmp $
+
+;; -------------------------------------------------------------------------- ;;
+
+%include "a20.asm"
