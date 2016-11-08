f7c04180b5f97f5373b5ffa381bc07a41e6acb6f
Guido <guidoteren@gmail.com>
2016-11-08 16:11:16 -0300

    defines

--- tp3/src/defines.h
+++ tp3/src/defines.h
@@ -27,6 +27,12 @@
 /* Indices en la gdt */
 /* -------------------------------------------------------------------------- */
 #define GDT_IDX_NULL_DESC           0
+#define GDT_IDX_NULL_DESC           0x12 
+#define GDT_IDX_DATA_0				0x13
+#define GDT_IDX_DATA_3				0x14
+#define GDT_IDX_CODE_0				0X15
+#define GDT_IDX_CODE_3				0X16
+#define GDT_IDX_PANTALLA_KERNEL		0x17
 
 /* Direcciones de memoria */
 /* -------------------------------------------------------------------------- */
